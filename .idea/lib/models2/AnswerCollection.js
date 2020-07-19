d6360a6f432d1023650fc853462762684c1d1110
yilmazerd <yilmazerdem@gmail.com>
2020-07-19 02:51:13 -0400

    Update with new cnstructor

---
+++ .idea/lib/models2/AnswerCollection.js
@@ -0,0 +1,64 @@
+const AnswerCollectionBuilder = require('./AnswerCollectionBuilder');
+/**
+ * AnswerCollection class
+ */
+class AnswerCollection {
+    constructor(answerCollectionBuilder) {
+        // Throw error if the argument is not AnswerCollectionBuilder
+        if (!answerCollectionBuilder || !(answerCollectionBuilder instanceof AnswerCollectionBuilder.constructor)) {
+            throw 'Invalid answeCollectionBuilder'
+        }
+        this.answersMap = new Map(answerCollectionBuilder.getAnswers());
+        // We are 'injecting' the builder as a parameter here
+        // Suggested by this reference
+        // https://github.com/ryanmcdermott/clean-code-javascript#dependency-inversion-principle-dip
+        this.answerCollectionBuilder = answerCollectionBuilder;
+    }
+
+    size() {
+        return this.answersMap.size;
+    }
+
+    isEmpty() {
+        return this.answersMap.size === 0;
+    }
+
+    // Removes the key and creates a new answer collection
+    remove(k) {
+        let answerMapOut = new Map(this.answersMap);
+        console.log('deleting key:',k)
+        console.log(answerMapOut.get(k))
+        answerMapOut.delete(k)
+        return this.answerCollectionBuilder.withAnswers(answerMapOut).build();
+    }
+
+    getKeys() {
+        return this.answersMap.keys();
+    }
+
+    // method returns a boolean indicating whether an element with the specified key exists or not.
+    hasKey(k) {
+        return this.answersMap.hasKey(k);
+    }
+
+    findKeyMatches(keyRegex) {
+        // return an answer collection whose keys match to given regex pattern
+    }
+
+    removeKeyMatches(keyRegex) {
+        // return an answer collection with answers whose keys match to given regex pattern are removed from 'this'
+    }
+
+    // we will return answers in object format
+    getAnswers() {
+        let answersOut = [];
+        this.answersMap.forEach(function(value, key){
+            answersOut[key] = value;
+        })
+        return answersOut;
+    }
+
+}
+
+// Export the module.
+module.exports = AnswerCollection;
---
+++ .idea/lib/models2/AnswerCollectionBuilder.js
@@ -0,0 +1,48 @@
+/**
+ * AnswerCollectionBuilder class
+ */
+const AnswerCollection = require('./AnswerCollection');
+class AnswerCollectionBuilder {
+    constructor() {
+        this.answersMap = new Map();
+    }
+
+    withAnswers(answers) {
+        // we let the client build an answer collection builder with a map or with an array
+        if (answers instanceof Map)
+        {
+            console.log('Building answercollection out of a map')
+            this.answersMap = new Map([...this.answersMap, ...answers])
+            return this;
+        } else if (answers instanceof Array) {
+            console.log('Building answercollection out of an array / object')
+            let newMap = new Map();
+            Object.keys(answers).forEach(answerKey => {
+                newMap.set(answerKey, answers[answerKey]);
+            })
+            this.answersMap = new Map([...this.answersMap, ...newMap]);
+            return this;
+        } else {
+            console.log(typeof  answers)
+        throw 'Invalid answers type'
+        }
+    }
+
+    withAnswer(k,v) {
+        this.answersMap.set(k,v);
+        return this;
+    }
+
+    getAnswers() {
+        return this.answersMap;
+    }
+
+    build() {
+        let acOut =  new AnswerCollection(this);
+        this.answersMap = new Map();
+        return acOut;
+    }
+
+}
+
+module.exports = AnswerCollectionBuilder;
